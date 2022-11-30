e3fc133e8ad2125f08dfd0195c0315c3a94ab2a2
m.rita1650@gmail.com <m.rita1650@gmail.com>
2022-11-30 17:49:40 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Напишите номер строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Напишите номер колонны: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [rows,columns];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,12);
+        Console.Write(matrix[i,j]+" ");
+    }
+    Console.WriteLine();
+}
